1383055516518d61fba98c2114d83140c209cd12
shideh-kolahdooz <54565479+shideh-kolahdooz@users.noreply.github.com>
2020-04-04 14:20:10 +1100

    initial commit

---
+++ RandomScenarios/RandomScenarios/Greetings.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace RandomScenarios
+{
+    public static class Greetings
+    {
+        [FunctionName("Greetings")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var greetingMessage = "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.";
+
+            return greetingMessage != null
+                ? (ActionResult)new OkObjectResult($"Hello, {greetingMessage}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
